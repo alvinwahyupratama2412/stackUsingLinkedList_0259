80011e01bd7a151af0c10a1f64458ff2884b7104
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-05-23 08:25:17 +0700

    membuat bool isEmpty

--- Stack.cpp
+++ Stack.cpp
@@ -55,4 +55,9 @@ class Stack{
                 cout << endl;
             }//return the value of the top node
         }
+
+        //isEmpty operation: check id the stack is empty
+        bool isEmpty(){
+            return top == NULL;//return true if the top pointer is NULL, indicating
+        }
 };
