c6b32546cd9b2eedb7c30429222f6a00a4c2aeac
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-05-23 08:09:21 +0700

    membuat cout popped value

--- Stack.cpp
+++ Stack.cpp
@@ -38,5 +38,7 @@ class Stack{
             
             Node *temp = top; //create a temporary pointer to the top node
             top= top->next; //Update the top pointer to the next node
+            cout << "Popped value :  "<< top->data << endl;
+            
         }
 };
